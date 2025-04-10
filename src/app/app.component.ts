a3dfd395f2aeda8087b5a3de65605f988745c022
Huzaifa Tariq <122553707+huzaifa898@users.noreply.github.com>
2025-04-10 10:07:59 +0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroComponent } from './hero/hero.component'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // 
+  imports: [RouterOutlet, HeroComponent], 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'landing-page';
+  logoPath = 'assets/Logo.png';  // Define logo path
+  
 }
